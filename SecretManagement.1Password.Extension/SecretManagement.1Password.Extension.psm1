6b39cebc92acabccd28bb336b7ed2f1cb1220e22
bstets1 <52285717+bstets1@users.noreply.github.com>
2022-07-11 08:56:57 -0400

    Update SecretManagement.1Password.Extension.psm1

--- SecretManagement.1Password.Extension/SecretManagement.1Password.Extension.psm1
+++ SecretManagement.1Password.Extension/SecretManagement.1Password.Extension.psm1
@@ -98,37 +98,19 @@ function Get-Secret {
         [Parameter()]
         [hashtable] $AdditionalParameters
     )
-    $totp = -1
-    $item = & op item get $Name --fields username,password,one-timepassword --vault $VaultName | ConvertFrom-Json -AsHashtable
-    if (-not [string]::IsNullOrEmpty($item["one-timepassword"]) )
-    {
-        $totp = & op item get --otp $Name --vault $VaultName 2>$nul
-    }
+    
+    $username = & op item get $Name --fields username --vault $VaultName --format=json | ConvertFrom-Json -AsHashtable
+    $password = & op item get $Name --fields password --vault $VaultName --format=json | ConvertFrom-Json -AsHashtable
+    
+    [securestring]$secureStringPassword = ConvertTo-SecureString $password.value -AsPlainText -Force
+    
+    $output = [PSCredential]::new(
+        $username.value,
+        $secureStringPassword
+    )
+    
 
-    if ( -not [string]::IsNullOrEmpty($item["password"]) ) {
-        [securestring]$secureStringPassword = ConvertTo-SecureString $item.password -AsPlainText -Force
-    }
-
-    $output = $null
-
-    if ([string]::IsNullOrEmpty($item["password"]) -and -not [string]::IsNullOrEmpty($item.username)) {
-        $output = @{UserName = $item.username}
-    } elseif
-    ([string]::IsNullOrEmpty($item.username)) {
-        $output = $secureStringPassword
-    }
-    else {
-        $output = [PSCredential]::new(
-            $item.username,
-            $secureStringPassword
-        )
-    }
-
-    if ($totp -gt -1) {
-            $output | Add-Member -MemberType ScriptMethod -Name totp -Value {& op item get --otp $Name --vault $VaultName}.GetNewClosure() -PassThru
-    } else {
-        $output
-    }
+    $output
 }
 
 function Set-Secret {
